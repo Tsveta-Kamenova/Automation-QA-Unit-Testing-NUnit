6f3ead68a7bf3a92d4dd6db9a5589f87073a3f4d
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-30 22:19:52 +0200

    more list methods unit testing

--- 07.2.Resources-Unit-Testing-Arrays-and-Lists/TestApp.UnitTests/AdjacentEqualTests.cs
+++ 07.2.Resources-Unit-Testing-Arrays-and-Lists/TestApp.UnitTests/AdjacentEqualTests.cs
@@ -6,7 +6,6 @@ namespace TestApp.UnitTests;
 
 public class AdjacentEqualTests
 {
-    // TODO: finish test
     [Test]
     public void Test_Sum_InputIsNull_ShouldThrowArgumentException()
     {
@@ -14,9 +13,9 @@ public class AdjacentEqualTests
         List<int>? nullList = null;
 
         // Act & Assert
+        Assert.That(() => AdjacentEqual.Sum(nullList), Throws.ArgumentException);
     }
 
-    // TODO: finish test
     [Test]
     public void Test_Sum_InputIsEmptyList_ShouldReturnEmptyString()
     {
@@ -24,49 +23,87 @@ public class AdjacentEqualTests
         List<int> emptyList = new();
 
         // Act
+        string result = AdjacentEqual.Sum(emptyList);
 
         // Assert
+        Assert.That(result, Is.EqualTo(string.Empty));
     }
 
-    // TODO: finish test
     [Test]
     public void Test_Sum_NoAdjacentEqualNumbers_ShouldReturnOriginalList()
     {
         // Arrange
+        List<int> noAdjacentEqualsList = new() { 1, 2, 3, 4, 5};
 
         // Act
+        string result = AdjacentEqual.Sum(noAdjacentEqualsList);
 
         // Assert
-        //Assert.That(result, Is.EqualTo("1 2 3 4 5"));
+        Assert.That(result, Is.EqualTo("1 2 3 4 5"));
     }
 
     [Test]
     public void Test_Sum_AdjacentEqualNumbersExist_ShouldReturnSummedList()
     {
-        // TODO: finish test
+        // Arrange
+        List<int> AdjacentEqualsList = new() { 1, 1, 3, 3, 5 };
+
+        // Act
+        string result = AdjacentEqual.Sum(AdjacentEqualsList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("2 6 5"));
     }
 
     [Test]
     public void Test_Sum_AllNumbersAreAdjacentEqual_ShouldReturnSingleSummedNumber()
     {
-        // TODO: finish test
+        // Arrange
+        List<int> AdjacentEqualsList = new() { 1, 1, 1, 1 };
+
+        // Act
+        string result = AdjacentEqual.Sum(AdjacentEqualsList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("4"));
     }
 
     [Test]
     public void Test_Sum_AdjacentEqualNumbersAtBeginning_ShouldReturnSummedList()
     {
-        // TODO: finish test
+        // Arrange
+        List<int> AdjacentEqualsList = new() { 1, 1, 5, 7, 253 };
+
+        // Act
+        string result = AdjacentEqual.Sum(AdjacentEqualsList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("2 5 7 253"));
     }
 
     [Test]
     public void Test_Sum_AdjacentEqualNumbersAtEnd_ShouldReturnSummedList()
     {
-        // TODO: finish test
+        // Arrange
+        List<int> AdjacentEqualsList = new() { 5, 7, 253, 1, 1 };
+
+        // Act
+        string result = AdjacentEqual.Sum(AdjacentEqualsList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("5 7 253 2"));
     }
 
     [Test]
     public void Test_Sum_AdjacentEqualNumbersInMiddle_ShouldReturnSummedList()
     {
-        // TODO: finish test
+        // Arrange
+        List<int> AdjacentEqualsList = new() { 5, 7, 253, 253, 1, 2 };
+
+        // Act
+        string result = AdjacentEqual.Sum(AdjacentEqualsList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("5 7 506 1 2"));
     }
 }
