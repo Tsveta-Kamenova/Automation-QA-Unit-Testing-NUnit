e2b5c38b0147376f86777e1bd6b6343518f57d0e
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-30 12:12:56 +0200

    test arrays and lists

--- 07.2.Resources-Unit-Testing-Arrays-and-Lists/TestApp.UnitTests/EvenOddSubtractionTests.cs
+++ 07.2.Resources-Unit-Testing-Arrays-and-Lists/TestApp.UnitTests/EvenOddSubtractionTests.cs
@@ -24,23 +24,38 @@ public class EvenOddSubtractionTests
     public void Test_FindDifference_InputHasOnlyEvenNumbers_ShouldReturnEvenSum()
     {
         // Arrange
+        int[] evenArray = new int[2] { 4, 2 };
 
         // Act
-        int result;
+        int result = EvenOddSubtraction.FindDifference(evenArray);
 
         // Assert
-        //Assert.That(result, Is.EqualTo(20));
+        Assert.That(result, Is.EqualTo(6));
     }
 
     [Test]
-    public void Test_FindDifference_InputHasOnlyOddNumbers_ShouldReturnNegativeOddSum()
+    public void Test_FindDifference_InputHasOnlyOddNumbers_ShouldReturnOddSum()
     {
-        // TODO: finish the test
+        // Arrange
+        int[] oddArray = new int[3] { 1, 3, 7};
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(oddArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(11));
     }
 
     [Test]
     public void Test_FindDifference_InputHasMixedNumbers_ShouldReturnDifference()
     {
-        // TODO: finish the test
+        // Arrange
+        int[] oddArray = new int[5] { 1, 3, 7, 2, 4};
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(oddArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
     }
 }
