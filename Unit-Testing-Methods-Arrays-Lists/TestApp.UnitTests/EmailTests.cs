02ae1333060d53027f15bd224e2d90c78aeecce1
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-31 22:26:01 +0200

    unit tests

---
+++ Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/EmailTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+        // Arrange
+        string validEmail = "test@example.com";
+
+        // Act
+        bool result = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        // Arrange
+        string validEmail = "test@examp  le.com";
+
+        // Act
+        bool result = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        // Arrange
+        string validEmail = null;
+
+        // Act
+        bool result = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(false));
+    }
+}
---
+++ Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/FibonacciTests.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FibonacciTests
+{
+    [Test]
+    public void Test_CalculateFibonacci_NegativeInput()
+    {
+        // Arrange
+        int n = -1;
+
+        // Act & Assert
+        Assert.That(() => Fibonacci.CalculateFibonacci(n), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_CalculateFibonacci_ZeroInput()
+    {
+        // Arrange
+        int n = 0;
+
+        // Act
+        int result = Fibonacci.CalculateFibonacci(n);
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_CalculateFibonacci_InputIsOne()
+    {
+        // Arrange
+        int n = 1;
+
+        // Act
+        int result = Fibonacci.CalculateFibonacci(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+
+    [Test]
+    public void Test_CalculateFibonacci_PositiveInput()
+    {
+        // Arrange
+        int n = 3;
+
+        // Act
+        int result = Fibonacci.CalculateFibonacci(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
