0e999ee9f96022f327d79232ea6513b0284fce39
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-02 23:02:31 +0200

    unit tests

---
+++ 0.SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _0.SumFactorialEvenDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumResult = 0.0;
+
+            List<int> inputList = (Console.ReadLine() ?? "1").Split("").Select(int.Parse).ToList();
+
+            for (int i = 1; i <= inputList.Count; i++)
+            {
+                if (inputList[i] % 2 == 0)
+                {
+                    double currentResult = CalculateFactorial(inputList[i]);
+                    sumResult += currentResult;
+                }
+            }
+
+            Console.WriteLine(sumResult);
+        }
+
+        static double CalculateFactorial(double number)
+        {
+            double result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
